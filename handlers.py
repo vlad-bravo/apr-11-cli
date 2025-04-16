c6bf746d0cf8e04a3005916df8e1f37d76bccba0
vlad-bravo <17.bravo@gmail.com>
2025-04-16 16:18:25 +0500

    remove total_reqests from data, calculate in report

--- handlers.py
+++ handlers.py
@@ -2,7 +2,7 @@ from typing import Union, List
 
 
 def init() -> List[Union[int, List]]:
-    return [0, []]
+    return []
 
 
 def process(data: List[Union[int, List]], in_str: str) -> None:
@@ -12,12 +12,12 @@ def process(data: List[Union[int, List]], in_str: str) -> None:
             handler = parts[2]
         else:
             handler = parts[4]
-        for element in data[1]:
+        for element in data:
             if element[0] == handler:
                 break
         else:
             element = [handler, 0, 0, 0, 0, 0]
-            data[1].append(element)
+            data.append(element)
         if level == 'DEBUG':
             element[1] += 1
         elif level == 'INFO':
@@ -28,20 +28,18 @@ def process(data: List[Union[int, List]], in_str: str) -> None:
             element[4] += 1
         elif level == 'CRITICAL':
             element[5] += 1
-        data[0] += 1
 
 
 def merge(data_all: List[List[Union[int, List]]]) -> List[Union[int, List]]:
     result = data_all[0]
     for data in data_all[1:]:
-        result[0] += data[0]
-        for element in data[1]:
-            for result_element in result[1]:
+        for element in data:
+            for result_element in result:
                 if result_element[0] == element[0]:
                     break
             else:
                 result_element = [element[0], 0, 0, 0, 0, 0]
-                result[1].append(result_element)
+                result.append(result_element)
             result_element[1] += element[1]
             result_element[2] += element[2]
             result_element[3] += element[3]
@@ -51,16 +49,18 @@ def merge(data_all: List[List[Union[int, List]]]) -> List[Union[int, List]]:
 
 
 def result(data: List[Union[int, List]]) -> str:
-    lines = [f'Total requests: {data[0]}']
-    lines.append('')
+    total_requests = 0
+    lines = ['', '']    # Total requests + empty line
     lines.append('HANDLER              DEBUG     INFO      WARNING   ERROR     CRITICAL')
     total_debug, total_info, total_warning, total_error, total_critical = 0, 0, 0, 0, 0
-    for element in sorted(data[1], key=lambda x: x[0]):
+    for element in sorted(data, key=lambda x: x[0]):
         lines.append(f'{element[0]:<20} {element[1]:<9} {element[2]:<9} {element[3]:<9} {element[4]:<9} {element[5]:<9}')
         total_debug += element[1]
         total_info += element[2]
         total_warning += element[3]
         total_error += element[4]
         total_critical += element[5]
+        total_requests += sum(element[1:])
     lines.append(f'                     {total_debug:<9} {total_info:<9} {total_warning:<9} {total_error:<9} {total_critical:<9}')
+    lines[0] = f'Total requests: {total_requests}'
     return '\n'.join(lines)
--- tests/test_handlers.py
+++ tests/test_handlers.py
@@ -4,21 +4,21 @@ from handlers import init, process, merge, result
 
 
 def test_init() -> None:
-    assert init() == [0, []]
+    assert init() == []
 
 
 @pytest.mark.parametrize(
     'data, in_str, out_data',
     [
         (
-            [0, []],
+            [],
             '2025-03-28 12:44:46,000 INFO django.request: GET /api/v1/reviews/ 204 OK [192.168.1.59]',
-            [1, [['/api/v1/reviews/', 0, 1, 0, 0 ,0]]]
+            [['/api/v1/reviews/', 0, 1, 0, 0 ,0]]
         ),
         (
-            [0, []],
+            [],
             '2025-03-28 12:11:57,000 ERROR django.request: Internal Server Error: /admin/dashboard/ [192.168.1.29] - ValueError: Invalid input data',
-            [1, [['/admin/dashboard/', 0, 0, 0, 1, 0]]]
+            [['/admin/dashboard/', 0, 0, 0, 1, 0]]
         ),
     ]
 )
@@ -30,40 +30,28 @@ def test_process(data, in_str: str, out_data) -> None:
 def test_merge() -> None:
     data_all = [
         [
-            2,
-            [
-                ['/admin/login/', 0, 1, 0, 0 ,0],
-                ['/admin/dashboard/', 1, 0, 0, 0 ,0]
-            ]
+            ['/admin/login/', 0, 1, 0, 0 ,0],
+            ['/admin/dashboard/', 1, 0, 0, 0 ,0]
         ],
         [
-            3,
-            [
-                ['/api/v1/orders/', 0, 0, 1, 0 ,0],
-                ['/api/v1/checkout/', 0, 0, 0, 1 ,0],
-                ['/admin/login/', 0, 0, 0, 0 ,1]
-            ]
+            ['/api/v1/orders/', 0, 0, 1, 0 ,0],
+            ['/api/v1/checkout/', 0, 0, 0, 1 ,0],
+            ['/admin/login/', 0, 0, 0, 0 ,1]
         ]
     ]
     data = [
-        5,
-        [
-            ['/admin/login/', 0, 1, 0, 0 ,1],
-            ['/admin/dashboard/', 1, 0, 0, 0 ,0],
-            ['/api/v1/orders/', 0, 0, 1, 0 ,0],
-            ['/api/v1/checkout/', 0, 0, 0, 1 ,0]
-        ]
+        ['/admin/login/', 0, 1, 0, 0 ,1],
+        ['/admin/dashboard/', 1, 0, 0, 0 ,0],
+        ['/api/v1/orders/', 0, 0, 1, 0 ,0],
+        ['/api/v1/checkout/', 0, 0, 0, 1 ,0]
     ]
     assert merge(data_all) == data
 
 
 def test_result() -> None:
     data = [
-        2,
-        [
-            ['/api/v1/reviews/', 0, 1, 0, 0 ,0],
-            ['/admin/dashboard/', 0, 0, 0, 1, 0]
-        ]
+        ['/api/v1/reviews/', 0, 1, 0, 0 ,0],
+        ['/admin/dashboard/', 0, 0, 0, 1, 0]
     ]
     assert result(data) == """Total requests: 2
 
